8dda25b6874a32acbfba41f2138586f8c10d7407
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2021-03-15 14:34:07 +0000

    Source Cleanup

---
+++ weather settings/NTTR GOOD NIGHT.lua
@@ -0,0 +1,89 @@
+vdata = 
+{
+    atmosphere_type = 0,
+    wind = 
+    {
+        at8000 = 
+        {
+            speed = 14.9466,
+            dir = 50,
+        }, -- end of at8000
+        at2000 = 
+        {
+            speed = 10.8234,
+            dir = 30,
+        }, -- end of at2000
+        atGround = 
+        {
+            speed = 4.1232,
+            dir = 10,
+        }, -- end of atGround
+    }, -- end of wind
+    enable_fog = false,
+    season = 
+    {
+        temperature = 27,
+    }, -- end of season
+    visibility = 
+    {
+        distance = 80000,
+    }, -- end of visibility
+    type_weather = 0,
+    qnh = 757.682,
+    cyclones = 
+    {
+        [1] = 
+        {
+            rotation = -0.58208521593028,
+            centerZ = -123707.21386469,
+            pressure_spread = 897347.29345743,
+            ellipticity = 0.94907932695036,
+            pressure_excess = -1161,
+            centerX = -331273.66694967,
+        }, -- end of [1]
+        [2] = 
+        {
+            rotation = -0.58208521593028,
+            centerZ = -830858.09320347,
+            pressure_spread = 887942.2429883,
+            ellipticity = 0.94907932695036,
+            pressure_excess = 1021,
+            centerX = 598218.1949045,
+        }, -- end of [2]
+        [3] = 
+        {
+            rotation = -0.58208521593028,
+            centerZ = 535858.09320347,
+            pressure_spread = 887942.2429883,
+            ellipticity = 0.94907932695036,
+            pressure_excess = 1021,
+            centerX = -1198218.1949045,
+        }, -- end of [3]
+    }, -- end of cyclones
+    name = "NTTR GOOD NIGHT",
+    fog = 
+    {
+        thickness = 0,
+        visibility = 0,
+    }, -- end of fog
+    groundTurbulence = 9.144,
+    dust_density = 0,
+    enable_dust = false,
+    clouds = 
+    {
+        thickness = 200,
+        density = 0,
+        base = 300,
+        iprecptns = 0,
+    }, -- end of clouds
+} -- end of vdata
+dtime = 
+{
+    date = 
+    {
+        Day = 4,
+        Year = 2020,
+        Month = 5,
+    }, -- end of date
+    start_time = 68400,
+} -- end of dtime
