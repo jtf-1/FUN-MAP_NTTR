75e1fbaaaead33cf24966ad4a7af5bdbc96313be
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2023-09-23 14:55:16 +0100

    Update missionsrs_data.lua

--- scripts/dynamic/missionsrs_data.lua
+++ scripts/dynamic/missionsrs_data.lua
@@ -16,19 +16,19 @@ if not MISSIONSRS then
 end
 
 -- table of values for missionsrs to use this miz. Overrides default values.
-MISSIONSRS.srsPath = "C:/Program Files/DCS-SimpleRadio-Standalone" -- default path to SRS install directory if setting file is not avaialable "C:/Program Files/DCS-SimpleRadio-Standalone"
-MISSIONSRS.srsPort = 5002                                          -- default SRS port to use if settings file is not available
-MISSIONSRS.msg = "No Message Defined!"                             -- default message if text is nil
+-- MISSIONSRS.srsPath = "C:/Program Files/DCS-SimpleRadio-Standalone" -- default path to SRS install directory if setting file is not avaialable "C:/Program Files/DCS-SimpleRadio-Standalone"
+-- MISSIONSRS.srsPort = 5002                                          -- default SRS port to use if settings file is not available
+-- MISSIONSRS.msg = "No Message Defined!"                             -- default message if text is nil
 MISSIONSRS.freqs = "243,251,327,377.8,30"                          -- transmit on guard, CTAF, NTTR TWR, NTTR BLACKJACK and 30FM as default frequencies
-MISSIONSRS.modulations = "AM,AM,AM,AM,FM"                          -- default modulation (count *must* match qty of freqs)
-MISSIONSRS.vol = "1.0"                                             -- default to full volume
-MISSIONSRS.name = "Server"                                         -- default to server as sender
-MISSIONSRS.coalition = 0                                           -- default to spectators
-MISSIONSRS.vec3 = nil                                              -- point from which transmission originates
-MISSIONSRS.speed = 2                                               -- speed at which message should be played
-MISSIONSRS.gender = "female"                                       -- default gender of sender
-MISSIONSRS.culture = "en-US"                                       -- default culture of sender
-MISSIONSRS.voice = ""                                              -- default voice to use
+-- MISSIONSRS.modulations = "AM,AM,AM,AM,FM"                          -- default modulation (count *must* match qty of freqs)
+-- MISSIONSRS.vol = "1.0"                                             -- default to full volume
+-- MISSIONSRS.name = "Server"                                         -- default to server as sender
+-- MISSIONSRS.coalition = 0                                           -- default to spectators
+-- MISSIONSRS.vec3 = nil                                              -- point from which transmission originates
+-- MISSIONSRS.speed = 2                                               -- speed at which message should be played
+-- MISSIONSRS.gender = "female"                                       -- default gender of sender
+-- MISSIONSRS.culture = "en-US"                                       -- default culture of sender
+-- MISSIONSRS.voice = ""                                              -- default voice to use
 
 if MISSIONSRS.Start then
 	MISSIONSRS:Start()
