d902ed5bf023406e36029819970e3b5606dc306b
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2023-09-23 14:56:08 +0100

    Update bfmacm_data.lua

--- scripts/dynamic/bfmacm_data.lua
+++ scripts/dynamic/bfmacm_data.lua
@@ -20,6 +20,21 @@ BFMACM.zonesNoSpawnName = { -- zones inside BFM/ACM zone within which adversarie
 	"zone_box",
 } 
 
+BFMACM.adversary = {
+	menu = { -- Adversary menu
+		{template = "ADV_F4", menuText = "Adversary A-4"},
+		{template = "ADV_MiG28", menuText = "Adversary MiG-28"},
+		{template = "ADV_Su27", menuText = "Adversary MiG-23"},
+		{template = "ADV_MiG23", menuText = "Adversary Su-27"},
+		{template = "ADV_F16", menuText = "Adversary F-16"},
+		{template = "ADV_F18", menuText = "Adversary F-18"},
+	},
+	range = {5, 10, 20}, -- ranges at which to spawn adversaries in nautical miles
+	spawn = {}, -- container for aversary spawn objects
+	defaultRadio = "377.8",
+}
+
+
 if BFMACM.Start then
 	BFMACM:Start()
 end
