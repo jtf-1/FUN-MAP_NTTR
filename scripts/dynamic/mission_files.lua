b354d7f567d0e311c3c29e424ae711d94b137ac1
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2023-09-23 14:56:16 +0100

    Update mission_files.lua

--- scripts/dynamic/mission_files.lua
+++ scripts/dynamic/mission_files.lua
@@ -2,26 +2,28 @@ env.info( "[JTF-1] mission_files" )
 __JTFLoader.Include( 'lib/Moose.lua' )                  -- library file
 __JTFLoader.Include( 'lib/skynet-iads-compiled.lua' )   -- library file
 __JTFLoader.Include( 'core/mission_init.lua' )          -- core file
+__JTFLoader.Include( 'mission_init_data.lua' )          -- core file
 __JTFLoader.Include( 'core/devcheck.lua' )              -- core file
 __JTFLoader.Include( 'disableai.lua' )
 __JTFLoader.Include( 'core/missionsrs.lua' )            -- core file
 __JTFLoader.Include( 'missionsrs_data.lua' )            -- core file
 __JTFLoader.Include( 'core/adminmenu.lua' )             -- core file
+__JTFLoader.Include( 'adminmenu_data.lua' )             -- core file
 __JTFLoader.Include( 'core/missiontimer.lua' )          -- core file
 __JTFLoader.Include( 'missiontimer_data.lua' )
-__JTFLoader.Include( 'core/missiletrainer.lua' )        -- core file
-__JTFLoader.Include( 'missiletrainer_data.lua' )
 __JTFLoader.Include( 'core/supportaircraft.lua' )       -- core file
 __JTFLoader.Include( 'supportaircraft_data.lua' )
 __JTFLoader.Include( 'core/staticranges.lua' )          -- core file
 __JTFLoader.Include( 'staticranges_data.lua' )
 __JTFLoader.Include( 'core/activeranges.lua' )         -- core file
 __JTFLoader.Include( 'activeranges_data.lua' )
-__JTFLoader.Include( 'movingtargets.lua' )
 __JTFLoader.Include( 'ecs.lua' )
 __JTFLoader.Include( 'core/bfmacm.lua' )                -- core file
 __JTFLoader.Include( 'bfmacm_data.lua' )
 __JTFLoader.Include( 'bvrgci.lua' )
+__JTFLoader.Include( 'core/missiletrainer.lua' )        -- core file
+__JTFLoader.Include( 'missiletrainer_data.lua' )
 __JTFLoader.Include( 'markspawn.lua' )
+__JTFLoader.Include( 'movingtargets.lua' )
 __JTFLoader.Include( 'core/Hercules_Cargo.lua' )        -- core file
 __JTFLoader.Include( 'core/mission_end.lua' )           -- core file
