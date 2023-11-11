6179c6b4f3beefe00b8a9c052ee399c3d2ba6b2f
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2023-11-11 14:42:43 +0000

    Update mission_files.lua

--- scripts/dynamic/mission_files.lua
+++ scripts/dynamic/mission_files.lua
@@ -12,10 +12,13 @@ __JTFLoader.Include( 'adminmenu_data.lua' )             -- core file
 __JTFLoader.Include( 'core/missiontimer.lua' )          -- core file
 __JTFLoader.Include( 'missiontimer_data.lua' )
 __JTFLoader.Include( 'core/supportaircraft.lua' )       -- core file
+__JTFLoader.Include( 'core/supportaircraft_templates.lua' )       -- core file
 __JTFLoader.Include( 'supportaircraft_data.lua' )
-__JTFLoader.Include( 'core/staticranges.lua' )          -- core file
+--__JTFLoader.Include( 'core/staticranges.lua' )        -- core file
+__JTFLoader.Include( 'test/staticranges.lua' )        -- core file
 __JTFLoader.Include( 'staticranges_data.lua' )
-__JTFLoader.Include( 'core/activeranges.lua' )         -- core file
+--__JTFLoader.Include( 'core/activeranges.lua' )          -- core file
+__JTFLoader.Include( 'test/activeranges.lua' )          -- core file
 __JTFLoader.Include( 'activeranges_data.lua' )
 __JTFLoader.Include( 'ecs.lua' )
 __JTFLoader.Include( 'core/bfmacm.lua' )                -- core file
@@ -23,7 +26,8 @@ __JTFLoader.Include( 'bfmacm_data.lua' )
 __JTFLoader.Include( 'bvrgci.lua' )
 __JTFLoader.Include( 'core/missiletrainer.lua' )        -- core file
 __JTFLoader.Include( 'missiletrainer_data.lua' )
-__JTFLoader.Include( 'markspawn.lua' )
+__JTFLoader.Include( 'core/markspawn.lua' )             -- core file
+__JTFLoader.Include( 'core/markspawn_templates.lua' )   -- core file
 __JTFLoader.Include( 'movingtargets.lua' )
 __JTFLoader.Include( 'core/Hercules_Cargo.lua' )        -- core file
 __JTFLoader.Include( 'core/mission_end.lua' )           -- core file
