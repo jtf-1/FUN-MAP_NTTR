1d2585ce807dea748213427199907f467206152c
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2023-11-22 14:42:04 +0000

    Update missionranges_data.lua

--- scripts/dynamic/missionranges_data.lua
+++ scripts/dynamic/missionranges_data.lua
@@ -5,23 +5,23 @@ env.info( "[JTF-1] activeranges_data" )
 --
 -- This file MUST be loaded AFTER activeranges.lua
 --
--- These values are specific to the miz and will override the default values in ACTIVERANGES.default
+-- These values are specific to the miz and will override the default values in MISSIONRANGES.default
 --
 
 -- Error prevention. Create empty container if module core lua not loaded.
-if not ACTIVERANGES then 
-	_msg = "[JTF-1 ACTIVERANGES] CORE FILE NOT LOADED!"
+if not MISSIONRANGES then 
+	_msg = "[JTF-1 MISSIONRANGES] CORE FILE NOT LOADED!"
 	BASE:E(_msg)
-	ACTIVERANGES = {}
+	MISSIONRANGES = {}
 end
 
 
-ACTIVERANGES.activeatstart = false -- if true, spawn targets with AI inactive
-ACTIVERANGES.useSRS = true -- set to false to disable use of SRS for this module in this miz
---ACTIVERANGES.rangeRadio = "377.8" -- radio frequency over which to broadcast Active Range messages
+MISSIONRANGES.activeatstart = false -- if true, spawn targets with AI inactive
+MISSIONRANGES.useSRS = true -- set to false to disable use of SRS for this module in this miz
+--MISSIONRANGES.rangeRadio = "377.8" -- radio frequency over which to broadcast Active Range messages
 
 
 -- start the mission timer
-if ACTIVERANGES.Start then
-	ACTIVERANGES:Start()
+if MISSIONRANGES.Start then
+	MISSIONRANGES:Start()
 end
