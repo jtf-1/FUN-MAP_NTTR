8dd26b3a462b791211c3bda4955b1b4edd73206c
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2023-12-19 18:01:26 +0000

    Update disableai.lua

--- scripts/dynamic/disableai.lua
+++ scripts/dynamic/disableai.lua
@@ -1,22 +1,54 @@
 env.info( "[JTF-1] disableai" )
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
---- Disable AI for ground targets and FAC
+--- Disable AI for ground targets
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-local setGroupGroundActive = SET_GROUP:New():FilterActive():FilterCategoryGround():FilterOnce()
+DISABLEAI = {}
+DISABLEAI.traceTitle = "[JTF-1 DISABLEAI] "
+DISABLEAI.setGroupGroundActive = SET_GROUP:New()
+  :FilterActive()
+  :FilterCategoryGround()
+  :FilterOnce()
+--local setGroupGroundActive = SET_GROUP:New():FilterActive():FilterCategoryGround():FilterOnce()
 
--- Prefix for groups for which AI should NOT be disabled
-local excludeAI = "BLUFOR"
+-- table of Prefixes for groups for which AI should NOT be disabled
+DISABLEAI.excludeAI = {"BLUFOR", "FAC", "JTAC", "66%-"}
+--local excludeAI = "BLUFOR"
 
-setGroupGroundActive:ForEachGroup(
+
+DISABLEAI.setGroupGroundActive:ForEachGroup(
   function(activeGroup)
-    if not string.find(activeGroup:GetName(), excludeAI) then
+    local _msg
+    -- name of the group we're checking
+    local activeGroupName = activeGroup:GetName()
+    -- list of group name prefixes to exclude
+    local excludeAI = DISABLEAI.excludeAI
+    -- flag to trigger disabling AI
+    local disableGroup = true
+
+    -- check if group name prefix is in exclusion list
+    for _, stringExclude in pairs(excludeAI) do
+
+      if string.find(activeGroupName, stringExclude) then
+        _msg = string.format("%sSkipping group: %s", DISABLEAI.traceTitle, activeGroupName)
+        disableGroup = false
+        break
+      end
+
+    end
+
+    -- Disable AI if group was not in exclusion list
+    if disableGroup then
+      local _msg = string.format("%sDisable group: %s", DISABLEAI.traceTitle, activeGroupName)
       activeGroup:SetAIOff()
-    end      
+    end
+
+    BASE:T(_msg)
+
   end
 )
 
 -- remove set object
-setGroupGroundActive = nil
+DISABLEAI.setGroupGroundActive = nil
 
 ---  END DISABLE AI
